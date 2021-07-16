fb5b83f04ebfd3b204dc22ee1572004038adbbe3
Samith <samithwijesinha@gmail.com>
2021-07-16 10:56:14 +0530

    first commit

---
+++ Smith's tempreature/Smith_s_tempreatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smith_s_tempreatureApp.swift
+//  Smith's tempreature
+//
+//  Created by Samith Wijesinghe on 2021-07-16.
+//
+
+import SwiftUI
+
+@main
+struct Smith_s_tempreatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
